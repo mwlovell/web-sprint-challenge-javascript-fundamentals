50d02ff90a9c7737aac682f32e2fe148c0a73608
Mike Lovell <mike@Carlys-MacBook-Air.local>
2021-12-17 06:32:49 -0800

    6 passing

--- index.js
+++ index.js
@@ -103,9 +103,15 @@ console.log(lowPopulationAnimals(zooAnimals))
   Remember the reduce method takes two arguments: a callback (which itself takes two args - the accumulator and the item), and an initial value for the count.
   */
 
-  function USApop(/*Your Code Here*/){
-    /*Your Code Here*/
+  function USApop(zooAnimals){
+    let singleValue = []
+     singleValue = zooAnimals.reduce((accumulator, item) => {   
+      return accumulator + item.population  
+      },0);
+      return singleValue
   }
+  
+  console.log(USApop(zooAnimals))
   
   
   // 🦁🦁🦁 Callbacks 🦁🦁🦁  
